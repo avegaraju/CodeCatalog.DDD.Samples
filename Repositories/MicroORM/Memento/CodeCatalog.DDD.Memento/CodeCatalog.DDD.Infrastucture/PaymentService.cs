e2e6827a14d234e4fae1f34af8deb9408ea2a338
Ashish Vegaraju <ashish_vegaraju@yahoo.com>
2017-11-29 23:54:24 +0100

    adding comment to payment service class that its a dummy implementation

--- Repositories/MicroORM/Memento/CodeCatalog.DDD.Memento/CodeCatalog.DDD.Infrastucture/PaymentService.cs
+++ Repositories/MicroORM/Memento/CodeCatalog.DDD.Memento/CodeCatalog.DDD.Infrastucture/PaymentService.cs
@@ -5,10 +5,15 @@ using CodeCatalog.DDD.Domain.Types;
 
 namespace CodeCatalog.DDD.Infrastucture
 {
+    /// <summary>
+    /// This is a dummy implementation of Payment Service.
+    /// </summary>
     public class PaymentService: IPaymentService
     {
         public PaymentReference Pay(decimal amount, Guid orderId)
         {
+            //Payment is successful always!
+
             return new PaymentReference()
                    {
                        TransactionId = Guid.NewGuid()
